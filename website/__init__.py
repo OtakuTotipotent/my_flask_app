5bc6e0764ae94409eaeda4dbb3accce70c6a9a9d
Afnan Muhammad <otakutotipotent@gmail.com>
2025-09-08 18:15:39 +0500

    Add Flask blueprint and improve item list UI
    Introduced a new 'views' blueprint for routing and registered it in the Flask app. Updated the index template to use a new 'list' variable and added an '+ Add' button. Enhanced the CSS for better styling of the item list and selection effects.

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "dev_secret_key"
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
+
     return app
---
+++ website/views.py
@@ -0,0 +1,11 @@
+from flask import render_template, Blueprint
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+def index():
+    return render_template(
+        "index.html",
+        list=["Afnan", "Hassan", "Ali", "Usman", "Hussain", "Saad", "Umar"],
+    )
